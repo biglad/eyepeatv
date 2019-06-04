ac261c194721b300b6ed1ad2f6c215f747b4e527
biglad <biglad@mgawow.co.uk>
2019-06-04 11:44:55 +0100

    add core mod
    add core mod

---
+++ zips/script.eptv.core/addon.py
@@ -0,0 +1,34 @@
+import time
+import xbmc
+import os
+import xbmcgui
+import urllib2
+import xbmcaddon
+import subprocess
+import platform
+import re
+import random
+import string
+from urllib2 import urlopen
+
+
+
+__addon__ = xbmcaddon.Addon()
+__addonname__ = __addon__.getAddonInfo('name')
+__icon__ = __addon__.getAddonInfo('icon')
+xbmc.executebuiltin("Notification(EyePeaTV, Checking For Updates,5000,"+__icon__ +")")
+
+def UpdateCheck():
+    xbmc.executebuiltin('xbmc.UpdateAddonRepos')
+    xbmc.executebuiltin('xbmc.UpdateLocalAddons')
+    xbmc.executebuiltin("XBMC.AlarmClock('MTVBCS',XBMC.RunAddon(script.cachesystem),60,silent)")
+    return
+            
+
+    
+
+UpdateCheck()
+
+
+
+    
---
+++ zips/script.eptv.core/startup.py
@@ -0,0 +1,10 @@
+import time
+import xbmc
+import os
+import xbmcgui
+import urllib2
+ 
+xbmc.executebuiltin("XBMC.AlarmClock('MTVBCS',XBMC.RunAddon(eyepeatv buold data),5,silent)")
+
+
+	

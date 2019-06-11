c8df04c4517c406ebfb4582e2e042baaadca727c
biglad <biglad@mgawow.co.uk>
2019-06-11 21:57:40 +0100

    update Jserge stuff

--- zips/script.module.resolveurl/lib/resolveurl/plugins/holavid.py
+++ zips/script.module.resolveurl/lib/resolveurl/plugins/holavid.py
@@ -1,6 +1,9 @@
-'''
-    resolveurl Kodi plugin
-    Copyright (C) 2019
+"""
+    OVERALL CREDIT TO:
+        t0mm0, Eldorado, VOINAGE, BSTRDMKR, tknorris, smokdpi, TheHighway
+
+    resolveurl XBMC Addon
+    Copyright (C) 2011 t0mm0
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,37 +17,35 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
+"""
 import re
 from lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class VeryStreamResolver(ResolveUrl):
-    name = "verystream"
-    domains = ["verystream.com"]
-    pattern = '(?://|\.)(verystream\.com)/(?:stream|e)/([a-zA-Z0-9]+)'
+class HolaVidResolver(ResolveUrl):
+    name = "holavid"
+    domains = ['holavid.com']
+    pattern = '(?://|\.)(holavid\.com)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT, 'Referer': 'https://verystream.com'}
-        response = self.net.http_GET(web_url, headers=headers)
-        html = response.content
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        html = self.net.http_GET(web_url, headers=headers).content
 
         if html:
-            regex = '(%s~[~.a-zA-Z0-9]+)' % media_id
-            videolink = re.search(regex, html)
-            if videolink:
-                source = 'https://verystream.com/gettoken/%s?mime=true' % videolink.group(1)
-                headers.update({'Referer': web_url})
-                return source + helpers.append_headers(headers)
+            _srcs = re.search(r'sources\s*:\s*\[(.+?)\]', html)
+            if _srcs:
+                srcs = helpers.scrape_sources(_srcs.group(1), patterns=['''["'](?P<url>http[^"']+)'''], result_blacklist=['.m3u8'])
+                if srcs:
+                    headers.update({'Referer': web_url})
+                    return helpers.pick_source(srcs) + helpers.append_headers(headers)
 
-        raise ResolverError("Could not locate video")
+        raise ResolverError('Unable to locate link')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'https://verystream.com/e/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')

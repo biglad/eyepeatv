83ec4f218d0024f03caaef06c2b3bbc9d254fdb5
biglad <biglad@mgawow.co.uk>
2019-06-12 23:04:32 +0100

    update installer
    update installer

--- zips/script.vistatv-installer/service.py
+++ zips/script.vistatv-installer/service.py
@@ -15,7 +15,7 @@ HOME        = xbmc.translatePath('special://home/userdata/')
 done      =  os.path.join(HOME, 'done1.xml')
 if os.path.exists(done):
     exit()
-update = xbmcgui.Dialog().yesno("[COLOR tomato]VistaTV Premium Installer[/COLOR]","[COLOR yellow]Do You Want to install Now?[/COLOR]","[COLOR turquoise]This is a paid for service!!!![/COLOR]" ,"[COLOR turquoise]Have your auth code ready!!!![/COLOR]")
+update = xbmcgui.Dialog().yesno("[COLOR tomato]EyePeaTV Build Installer[/COLOR]","[COLOR yellow]Do You Want to install Now?[/COLOR]","[COLOR turquoise]The IPTV need paying for, rest it free[/COLOR]" ,"[COLOR turquoise]www.epyv.co.uk for the IPTV side[/COLOR]")
 if update:
     xbmc.executebuiltin("Notification(PLEASE WAIT, [B][COLOR=gold]SYSTEM IS STARTING UP[/COLOR] -- [COLOR=green]PLEASE WAIT[/COLOR][/B],3000,)")
     xbmc.sleep(2000)

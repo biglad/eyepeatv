80c2319d0f4ef2fc648eeac205851640b1365b76
biglad <biglad@mgawow.co.uk>
2019-06-26 17:17:41 +0100

    fix login issue

--- zips/plugin.video.eyepeatv/default.py
+++ zips/plugin.video.eyepeatv/default.py
@@ -29,8 +29,8 @@ FANART = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'f
 APKS = base64.b64decode("aHR0cDovL2ZhYmlwdHYuY29tL2Fwa3MvbmV3YXBrcy50eHQ=")
 HOME =  xbmc.translatePath('special://home/')
 buildfile = "version.txt"
-lehekylg= base64.b64decode("aHR0cDovL2dvdGRhcmsuY29t") #DM
-#lehekylg= base64.b64decode("aHR0cDovL2VwdHYuY28udWs=") #EPTV
+#lehekylg= base64.b64decode("aHR0cDovL2dvdGRhcmsuY29t") #DM
+lehekylg= base64.b64decode("aHR0cDovL2VwdHYuY28udWs=") #EPTV
 pordinumber="80"
 message = "VU5BVVRIT1JJWkVEIEVESVQgT0YgQURET04h"
 kasutajanimi=plugintools.get_setting("Username")
@@ -46,7 +46,7 @@ BASEURL = base64.b64decode("JXM6JXMvcGxheS5waHA/dT0lcyZwPSVzJnR5cGU9bTN1X3BsdXMm
 
 
 LOAD_LIVEchan = os.path.join( plugintools.get_runtime_path() , "resources" , "art/arch" )
-loginurl   = base64.b64decode("JXM6JXMvcGxheS5waHA/dT0lcyZwPSVzJnR5cGU9bTN1X3BsdXMmb3V0cHV0PXRz")%(lehekylg,pordinumber,kasutajanimi,salasona) #EPTV
+loginurl   = base64.b64decode("JXM6JXMvcGxheTIucGhwP3U9JXMmcD0lcyZ0eXBlPW0zdV9wbHVzJm91dHB1dD1tM3U4")%(lehekylg,pordinumber,kasutajanimi,salasona) #EPTV
 #loginurl   = base64.b64decode("JXM6JXMvZ2V0LnBocD91c2VybmFtZT0lcyZwYXNzd29yZD0lcyZ0eXBlPW0zdV9wbHVzJm91dHB1dD10cw==")%(lehekylg,pordinumber,kasutajanimi,salasona) #DM
 #xbmc.log(loginurl,2)
 webversion = urllib2.urlopen('http://eptv.co.uk/version.txt').read()

20ddff28c8e146b6ed491f2d7d135d71f82dd538
biglad <biglad@mgawow.co.uk>
2019-07-01 14:12:32 +0100

    updte core

--- zips/script.eptv.core/startup.py
+++ zips/script.eptv.core/startup.py
@@ -3,8 +3,22 @@ import xbmc
 import os
 import xbmcgui
 import urllib2
+
+import platform
+
+KODIV          = float(xbmc.getInfoLabel("System.BuildVersion")[:4])
+
+
+if KODIV < 18.3:
+    dialog = xbmcgui.Dialog()
+    dialog.ok("[COLOR=white][B]EPTV[/COLOR][/B]","Your Kodi is needs updating","Your Version = "+str(KODIV),"Press OK to Continue and udpate ASAP")
+
+xbmc.executebuiltin('xbmc.UpdateAddonRepos')
+xbmc.executebuiltin('xbmc.UpdateLocalAddons')
  
 xbmc.executebuiltin("XBMC.AlarmClock('MTVBCS',XBMC.RunAddon(script.eptv.core),5,silent)")
 
+Main()
 
-	
+
+    

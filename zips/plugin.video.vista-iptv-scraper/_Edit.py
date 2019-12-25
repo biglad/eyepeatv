4a8dfde85f6952a112718ea47b13ef0472ec7e07
biglad <biglad@mgawow.co.uk>
2019-12-25 17:07:41 +0000

    update menu location

--- zips/plugin.video.vista-iptv-scraper/_Edit.py
+++ zips/plugin.video.vista-iptv-scraper/_Edit.py
@@ -4,7 +4,7 @@ import xbmc
 HOME     = xbmc.translatePath('special://userdata/')
 file     = os.path.join(HOME, '24-7.xml')
 
-MainBase = 'http://vistatv.me/buildrepo-1/to-be-cleaned-up-ed/super%20old%20stuff/9534958459845345/sdfdsfdsfdsfsdfsfdsf/1/2/lol/wtf/987654321999999.xml'
+MainBase = 'http://eptv.co.uk/987654321999999.xml'
 MainBase2 = file
 addon = xbmcaddon.Addon('plugin.video.vista-iptv-scraper')
 

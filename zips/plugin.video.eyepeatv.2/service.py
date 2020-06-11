aa65d5467119ef79c5c3325afe21e1c7f4989d4b
biglad <biglad@mgawow.co.uk>
2020-06-11 10:32:04 +0100

    ...
    ...

--- zips/plugin.video.eyepeatv.2/service.py
+++ zips/plugin.video.eyepeatv.2/service.py
@@ -1,14 +1,15 @@
 import xbmc,xbmcplugin,xbmcgui,re,os,xbmcaddon,sys,base64,time,urllib2,string,logging,array,news,shutil,plugintools
-AddonID = 'plugin.video.eyepeatv.2'
-FabAddon = xbmcaddon.Addon('plugin.video.eyepeatv.2')
-Username=plugintools.get_setting("Username")
-Password=plugintools.get_setting("Password")
+AddonID = 'plugin.video.eyepeatv'
+FabAddon = xbmcaddon.Addon('plugin.video.eyepeatv')
+Username = FabAddon.getSetting("Username")
+Password = FabAddon.getSetting("Password")
 PVRon = FabAddon.getSetting("PVRUpdater")
-lehekylg= base64.b64decode("aHR0cDovL2dvbGQuZXB0di5jby51aw==") #NEW
+lehekylg = base64.b64decode("aHR0cDovL2dvbGQuZXB0di5jby51aw==")
 pordinumber = base64.b64decode("MjA4Ng==")
-EPGurl = base64.b64decode("JXM6JXMvZ2V0LnBocD91c2VybmFtZT0lcyZwYXNzd29yZD0lcyZ0eXBlPW0zdV9wbHVzJm91dHB1dD10cw==")%(lehekylg,pordinumber,Username,Password)
+EPGurl = base64.b64decode("JXM6JXMveG1sdHYucGhwP3VzZXJuYW1lPSVzJnBhc3N3b3JkPSVz")%(lehekylg,pordinumber,Username,Password)
 NewPVR = xbmc.translatePath('special://home/userdata/addon_data/pvr.iptvsimple/EyePeaTV.m3u8')
 iVueRepo = xbmc.translatePath('special://home/addons/xbmc.repo.ivueguide')
+iVueRepo2 = xbmc.translatePath('special://home/addons/script.module.t2kfanriffictltw')
 dialog = xbmcgui.Dialog()
 Duration=FabAddon.getSetting("FinalDuration")
 if Duration == '':
@@ -16,6 +17,9 @@ if Duration == '':
 
 if os.path.exists(iVueRepo):
 	shutil.rmtree(iVueRepo)
+    
+if os.path.exists(iVueRepo2):
+	shutil.rmtree(iVueRepo2)
 
 def Check():
 	if PVRon == 'true':
@@ -25,19 +29,10 @@ def Check():
 		time.sleep(1)
 		try:
 			f = open(NewPVR, 'a')
-			data = ""
 			UserList = base64.b64decode("JXM6JXMvZ2V0LnBocD91c2VybmFtZT0lcyZwYXNzd29yZD0lcyZ0eXBlPW0zdV9wbHVzJm91dHB1dD10cw==")%(lehekylg,pordinumber,Username,Password)
-			user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-			headers = {'User-Agent': user_agent}
-			req = urllib2.Request(UserList, data, headers)
-			response = urllib2.urlopen(req)
-			link = response.read()
-			link = link.replace('\n','') #.replace('\r','&split&')
-			
-			OutpuT = link
-			#link = open_url(UserList).replace('\n','').replace('\r','&split&')
-			#a,b = newlink = link.split('&split&#EXTINF:-1 tvg-id="" tvg-name="Adult XXX')
-			#OutpuT = a.replace("&split&","\n").replace("#EXTM3U","#EXTM3U\n")
+			link = open_url(UserList).replace('\n','').replace('\r','&split&')
+			a,b = newlink = link.split('&split&#EXTINF:-1 tvg-id="" tvg-name="Absolute 80')
+			OutpuT = a.replace("&split&","\n").replace("#EXTM3U","#EXTM3U\n")
 			f = open(NewPVR, 'a')
 			f.write(OutpuT)
 			if PVRon == 'false':
@@ -50,17 +45,10 @@ def Check():
 
 def open_url(url):
     try:
-        data = ""
-        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-        headers = {'User-Agent': user_agent}
-        req = urllib2.Request(url, data, headers)
+        req = urllib2.Request(url,headers={"Accept" : "application/xml","User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"})
         response = urllib2.urlopen(req)
-        link = response.read()
-		
-        #req = urllib2.Request(url,headers={"Accept" : "application/xml","User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"})
-        #response = urllib2.urlopen(req)
-        #link=response.read()
-        #response.close()
+        link=response.read()
+        response.close()
         return link
     except:quit()
 
